3a0aacc57e6f411a37750b8cc6a31261d62d069a
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-10-25 19:03:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("ready", async  => {
-setInterval(function(){
-client.channels.find('631178123609767995', 'idd').setName("W");
-client.channels.find('631178123609767995', 'idd').setName("We");
-client.channels.find('631178123609767995', 'idd').setName("Wel");
-client.channels.find('631178123609767995', 'idd').setName("Welc");
-client.channels.find('631178123609767995', 'idd').setName("Welco");
-client.channels.find('631178123609767995', 'idd').setName("Welcom");
-client.channels.find('631178123609767995', 'idd').setName("Welcome");
-client.channels.find('631178123609767995', 'idd').setName("Welcome T");
-client.channels.find('631178123609767995', 'idd').setName("Welcome To");
-client.channels.find('631178123609767995', 'idd').setName("Welcome To S");
-client.channels.find('631178123609767995', 'idd').setName("Welcome To Se");
-client.channels.find('631178123609767995', 'idd').setName("Welcome To Ser");
-client.channels.find('631178123609767995', 'idd').setName("Welcome To Serv");
-client.channels.find('631178123609767995', 'idd').setName("Welcome To Serve");
-client.channels.find('631178123609767995', 'idd').setName("Welcome To Server onix");
-  }, 5000);
-});
+client.on('ready',async () => {//TX console.log("Starting..");//TX let g = client.guilds.get("id"); // id server let c = g.channels.get("631184361529671691");// id channel if(c.type === 'onix') {//TX c.join();//TX setInterval(() => {//TX if(!g.me.voiceChannel) c.join(); }, 1);//Toxic Codes } else {//TX console.log('Failed To Join: \n The Channel Type isn "Listening."') } });
+
 
 
 client.login(process.env.BOT_TOKEN);
