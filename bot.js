5d1a37bf33743b9273a69c4ec70cca433995e8a2
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-10-25 18:39:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('message', message => {
- if(message.content.split(' ')[0] == prefix + 'dc') { 
- if (!message.channel.guild) return;
- message.guild.channels.forEach(m => {
- m.delete();
- });
- }
- if(message.content.split(' ')[0] == prefix + 'dr') { // delete all roles
- if (!message.channel.guild) return;
- message.guild.roles.forEach(m => {
- m.delete();
- });
- message.reply("`تم حذف جميع الرتب بنجاح`")
- }
- });
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('631178123609767995', 'idd').setName("W");
+client.channels.find('631178123609767995', 'idd').setName("We");
+client.channels.find('631178123609767995', 'idd').setName("Wel");
+client.channels.find('631178123609767995', 'idd').setName("Welc");
+client.channels.find('631178123609767995', 'idd').setName("Welco");
+client.channels.find('631178123609767995', 'idd').setName("Welcom");
+client.channels.find('631178123609767995', 'idd').setName("Welcome");
+client.channels.find('631178123609767995', 'idd').setName("Welcome T");
+client.channels.find('631178123609767995', 'idd').setName("Welcome To");
+client.channels.find('631178123609767995', 'idd').setName("Welcome To S");
+client.channels.find('631178123609767995', 'idd').setName("Welcome To Se");
+client.channels.find('631178123609767995', 'idd').setName("Welcome To Ser");
+client.channels.find('631178123609767995', 'idd').setName("Welcome To Serv");
+client.channels.find('631178123609767995', 'idd').setName("Welcome To Serve");
+client.channels.find('631178123609767995', 'idd').setName("Welcome To Server onix");
+  }, 5000);
+});
+
 
 client.login(process.env.BOT_TOKEN);
 
